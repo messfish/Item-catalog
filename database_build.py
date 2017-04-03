6803d10f9e839086f3b420369925213fe037e631
messfish <messfish@users.noreply.github.com>
2017-04-03 11:54:45 -0700

    Add files via upload

--- database_build.py
+++ database_build.py
@@ -10,6 +10,7 @@ Base = declarative_base()
 
 engine = create_engine('sqlite:///minilibrary.db')
 Base.metadata.create_all(engine)
+Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
